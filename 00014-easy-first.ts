73168946645d9dc4ab62202c902b254bd24d5e06
kechuang <wutkechuang@163.com>
2024-05-20 00:23:08 +0800

    day02: study

---
+++ 00014-easy-first.ts
@@ -0,0 +1,24 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
+];
+
+type errors = [
+  // @ts-expect-error
+  First<"notArray">,
+  // @ts-expect-error
+  First<{ 0: "arrayLike" }>
+];
+
+// ============= Your Code Here =============
+type First<T extends unknown[]> = T[0] extends T[number] ? T[0] : never;
+
+// 其他题解
+// type First<T extends unknown[]> = T extends [infer A, ...infer B] ? A : never;
+// type First<T extends unknown[]> = T extends [] ? never : T[0];
+// type First<T extends unknown[]> = T["length"] extends 0 ? never : T[0];
